8c46a07a9bcdb4af7f04abb4ec41c4c0c9920326
Gevorg Harutyunyan <gevharut@gmail.com>
2022-02-02 18:23:09 +0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div className='container mx-auto py-10'>
+      <h1 className='text-blue-400 text-center text-5xl'>
+        Hello Hasura!
+      </h1>
+    </div>
+  )
+}
+
+export default Home
